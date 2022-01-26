1366306e8e6529e7e4f23373743b762a5da92716
6AM <bergerbtw@gmail.com>
2022-01-26 10:45:39 +0100

    Added Unit Test for lazy route

---
+++ src/test/java/de/berger/BackendManagerTest.kt
@@ -0,0 +1,40 @@
+package de.berger
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.net.HttpURLConnection
+import java.net.URL
+
+internal class BackendManagerTest {
+    private fun sendGet(uri: String): ArrayList<String> {
+        val url = URL(uri)
+        val connection = url.openConnection() as HttpURLConnection
+
+        connection.requestMethod = "GET"
+        connection.connect()
+
+        val response = connection.inputStream.bufferedReader().readText()
+        return ArrayList(response.split("\n"))
+    }
+
+    @Test
+    fun checkIfBackendManagerStartsWithLazyRoute() {
+        Thread {
+            val backendManager = BackendManager()
+
+            backendManager.routing.lazyRoute("/") {
+                plain("test")
+            }
+
+            backendManager.run(3000)
+        }.start()
+
+        Thread.sleep(1000)
+
+        val response = sendGet("http://localhost:3000/")
+
+        println("Response: $response")
+
+        assertEquals("test", response[0])
+    }
+}
